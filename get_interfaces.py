095286056e844d674b6e5053453b3f96c7d0dd22
ClovisSecurity <126100008+ClovisSecurity@users.noreply.github.com>
2023-03-27 08:52:40 -0600

    getting more info about interfaces

--- get_interfaces.py
+++ get_interfaces.py
@@ -7,7 +7,6 @@ from ncclient import manager
 from ncclient.operations import RPCError
 from router_info import router
 from pprint import pprint
-
 import xmltodict
 from script35 import payload
 
@@ -35,20 +34,23 @@ with manager.connect(host=router["host"], port=router["port"], username=router["
             oper_status = interface['admin-status']
             shutdown_bool = True
             
-            if oper_status == "id-state-up":
+            if oper_status == "if-state-up":
                 shutdown_bool = False
+            else:
+                shutdown_bool = True
             
             ip = interface['ipv4']
             subnet = interface['ipv4-subnet-mask']
             output_acl = interface['output-security-acl']
             int_info = InterfaceInfo(name = name, desc = desc, vlans=[""], ip=(ip, subnet), mac = mac, shut=shutdown_bool)
+            
             print(int_info.name, int_info.desc, int_info.mac, shutdown_bool)
             print(int_info.ip[0], int_info.ip[1], output_acl)
             
             trunk_status = interface['ether-state']['negotiated-duplex-mode']
             int_trouble_data = InterfaceTrouble(name = interface['name'], trunk_status=trunk_status, speed=interface['speed'], mtu=interface['mtu'], last_change=interface['last-change'])
             
-            print(f"Name: {int_trouble_data.name}\nTrunk Status: {int_trouble_data.trunk_status}\nSpeed: {int_trouble_data.speed}\nMTU: {int_trouble_data.mtu}\nLast Change: {int_trouble_data.last_change}")
+            print(f"Name: {int_trouble_data.name}\nShutdown: {shutdown_bool}\nTrunk Status: {int_trouble_data.trunk_status}\nSpeed: {int_trouble_data.speed}\nMTU: {int_trouble_data.mtu}\nLast Change: {int_trouble_data.last_change}")
         
         exit()
     m.close_session()
