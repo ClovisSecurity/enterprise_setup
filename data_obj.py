df4bdd5e89dcef37cc93bb033fe01ea521f84ae9
ClovisSecurity <126100008+ClovisSecurity@users.noreply.github.com>
2023-03-27 09:00:07 -0600

    Updated RouterInfo

--- data_obj.py
+++ data_obj.py
@@ -6,21 +6,21 @@ from dataclasses import dataclass, field
 class InterfaceInfo:
     """Class stores information about an interface"""
     name: str = field(default_factory=str)
-    desc: str = field(default_factory=str)          # naming VLAN optional but important
+    desc: str = field(default_factory=str)                # naming VLAN optional but important
     vlans: list[str] = field(default_factory=list)
-    ip: str = field(default_factory=tuple)         # (ip, subnet)
+    ip: str = field(default_factory=tuple)                # (ip, subnet)
     mac: str = field(default_factory=str)
     shut: bool = field(default_factory=bool)              # true if shutdown. false if no shut.
     outside_bool: bool = field(default_factory=bool)      # used to determine if interface is outside or inside
-    link_type: str = field(default_factory=str)     # point-2-point/etc --optional
+    link_type: str = field(default_factory=str)           # point-2-point/etc --optional
     router_name: str = field(default_factory=str)
     
 @dataclass
 class RouterInfo:
     """Class stores general router info"""
     name: str = field(default_factory=str)
-    type: str = field(default_factory=str)     # ABR/CORE/ETC 
-    interface_info: list[InterfaceInfo] = field(default_factory=list)
+    type: str = field(default_factory=str)                                # ABR/CORE/ETC 
+    interfaces: list = field(default_factory=list)                        # list of interface names
     gateway: str = field(default_factory=str)
     desc: str = field(default_factory=str)
     loopback: str = field(default_factory=str)
@@ -31,10 +31,8 @@ class RouterInfo:
 class InterfaceTrouble:
     """Class with info useful for troubleshooting trunks"""
     name: str = field(default_factory=str)
-    trunk_status: str     # trunking
-    trunk_mode: str       # desirable/etc
-    duplex: str
-    speed: str
+    trunk_status: str = field(default_factory=str)     # trunking
+    speed: int = field(default_factory=int)
     mtu: int = field(default_factory=int) 
     access_mode: str = field(default_factory=str)      # trunk/access
     last_change: str = field(default_factory=str)
@@ -57,17 +55,17 @@ class OspfInfo:
     """Class for general OSPF info"""
     id: str =field(default_factory=str)
     proc: int = field(default=1)
-    area: list = field(default_factory=list)                      # If len(area) > 1 it's an ABR
+    area: list = field(default_factory=list)                         # If len(area) > 1 it's an ABR
     ip: tuple = field(default_factory=tuple)                         # (ip, subnet)
-    interfaces: list[str] = field(default_factory=list)                 # [eth1, eth0]
-    router_type: str = field(default_factory=str)                  # ABR
+    interfaces: list[str] = field(default_factory=list)              # [eth1, eth0]
+    router_type: str = field(default_factory=str)                    # ABR
     
 @dataclass
 class OspfNei:
     """Class for storing OSPF neighbor info"""
     id: str = field(default_factory=str)
     neighbors: list[str] = field(default_factory=list)
-    role: str = field(default_factory=str)# DR/BDR/etc
+    role: str = field(default_factory=str)                           # DR/BDR/etc
     
 # timers must be the same on neighboring routers
 @dataclass
@@ -86,16 +84,16 @@ class AccessList:
     name: str = field(default_factory=str)
     num: int = field(default_factory=int)
     interface: str = field(default_factory=str)
-    permit_ip: tuple = field(default_factory=tuple)# (permitted_ip, permitted_subnet)
-    deny_ip: tuple = field(default_factory=tuple)  # (deny_ip, deny_subnet)
+    permit_ip: tuple = field(default_factory=tuple)                 # (permitted_ip, permitted_subnet)
+    deny_ip: tuple = field(default_factory=tuple)                   # (deny_ip, deny_subnet)
     
 @dataclass
 class NatInfo:
     """Class for storing NAT-related info"""
-    outside_int: list[str] = field(default_factory=list) # list of outside interfaces
-    inside_int: list[str] = field(default_factory=list) # list of inside interfaces
-    type: str = field(default="PAT")    # PAT/STATIC/ETC
-    acl: str =field(default_factory=str)            # which ACL is being used on the inside
-    pool: str  = field(default_factory=str)            # name of NAT Pool
+    outside_int: list[str] = field(default_factory=list)            # list of outside interfaces
+    inside_int: list[str] = field(default_factory=list)             # list of inside interfaces
+    type: str = field(default="PAT")                                # PAT/STATIC/ETC
+    acl: str =field(default_factory=str)                            # which ACL is being used on the inside
+    pool: str  = field(default_factory=str)                         # name of NAT Pool
     
     
